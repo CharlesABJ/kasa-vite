d906a4ed208aff88e96160371b9a0519bf68fd80
Charles ABJ <biadjeu.charles@orange.fr>
2024-06-21 02:34:54 +0200

    Implementation de la page 404

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Error404(props) {
-  return <div></div>;
+function Error404() {
+  return (
+    <main className="Error404">
+      <h1>404</h1>
+      <h2>
+        Oups! La page que <br className="d-none-from-tablet" />
+        vous demandez n'existe pas.
+      </h2>
+      <Link to="/">Retourner sur la page d’accueil</Link>
+    </main>
+  );
 }
 
 export default Error404;
