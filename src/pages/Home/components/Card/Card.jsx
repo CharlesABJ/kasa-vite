aa44e64864b13f1530d743d1e7038463f22480ba
Charles ABJ <biadjeu.charles@orange.fr>
2024-06-22 14:11:20 +0200

    Amelioration de la logique de creation d'url pour les cards, dans le cas où dans le futur on souhaite indiquer la ville

--- src/pages/Home/components/Card/Card.jsx
+++ src/pages/Home/components/Card/Card.jsx
@@ -3,7 +3,14 @@ import { Link } from "react-router-dom";
 
 function Card({ cardData }) {
   return (
-    <Link to={`/logements/${cardData.id}`} className="Card">
+    <Link
+      to={`/logements${
+        cardData.city
+          ? `/${cardData.city.toLowerCase().split(" ").join("")}`
+          : ""
+      }/${cardData.id}`}
+      className="Card"
+    >
       <div className="overlay"></div>
       <img className="cover" src={cardData.cover} alt={cardData.title} />
       <h2>{cardData.title}</h2>
